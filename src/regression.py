255a90de54c893132342014f7f88f3ee23a1a4c6
Christian Fricke <christian@fricke.io>
2016-10-18 15:46:10 +0300

    Add baseline linear regression model

---
+++ src/regression.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from pandas.util.testing import debug
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+
+data_train = pd.read_csv('data/regression_dataset_training.csv', index_col=0)
+data_test = pd.read_csv('data/regression_dataset_testing.csv', index_col=0)
+
+x_train, y_train = data_train.drop('vote', axis=1), data_train.vote
+x_test = data_test
+
+def baseline(submit=False):
+    # data split
+    xtr, xte, ytr, yte = train_test_split(
+        x_train, y_train, test_size=0.2, random_state=42
+    )
+
+    # training
+    model = LinearRegression()
+    model.fit(xtr, ytr)
+
+    # testing
+    mse = np.mean((np.around(model.predict(xte)) - yte)**2)
+    print('MSE (baseline) : {}'.format(mse))
+
+    if submit:
+        evaluate(x_test, model, 'linear_base')
+
+
+def evaluate(x, model, name):
+    # predict and save for submission
+    y = pd.Series(np.around(model.predict(x)), index=x.index, name='vote')
+    y.to_csv('{}_submission.csv'.format(name), header=True)
