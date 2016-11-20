ea0f7f9a99bf7f034ac9f4e74aed1b86697cb499
nadan <nguyenandan@gmail.com>
2016-11-21 00:25:51 +0200

    Add KNN

--- src/classification.py
+++ src/classification.py
@@ -1,12 +1,36 @@
-class KNN():
-    def __int__(self, k=1):
+import numpy as np
+
+
+def euclidean_distance(a, b):
+    return np.dot(a - b, a - b)
+
+
+class KNN(object):
+    def __init__(self, k=1):
         self._model_x = None
         self._model_y = None
         self._k = k
+        self._num_obs = None
+
+    def __repr__(self):
+        return '{}<{}>'.format(self.__class__.__name__, self._k)
+
+    def euclidean_distance(self, a, b):
+        return np.dot(a - b, a - b)
 
     def fit(self, x, y):
         self._model_x = x.copy()
         self._model_y = y.copy()
+        self._num_obs = self._model_x.shape[0]
 
-    def predict(self, x, y):
-        pass
+    def predict(self, x):
+        pred = []
+        for i in range(x.shape[0]):
+            distance = []
+            for j in range(self._num_obs):
+                distance.append(euclidean_distance(x[i], self._model_x[j]))
+
+            top_k_label = self._model_y[np.argsort(distance)[::-1]][:self._k]
+            pred.append(np.mean(top_k_label))
+
+        return np.array(pred)
